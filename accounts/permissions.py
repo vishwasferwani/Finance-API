0db62d53d0a9127678c8e26ad7f0bd1f27690a79
Vishwas Ferwani <166019560+vishwasferwani@users.noreply.github.com>
2026-04-03 11:45:55 +0530

    Enhance permission classes to handle anonymous users
    Add checks for anonymous users in permission classes to handle attribute error 'AnonymousUser' object has no attribute 'role'

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -2,10 +2,14 @@ from rest_framework.permissions import BasePermission
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
+        if request.user.is_anonymous:
+            return False
         return request.user.role == 'admin'
 
 class IsAnalyst(BasePermission):
     def has_permission(self, request, view):
+        if request.user.is_anonymous:
+            return False
         return request.user.role in ['admin', 'analyst']
 
 class IsViewer(BasePermission):
