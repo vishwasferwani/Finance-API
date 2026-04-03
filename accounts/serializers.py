65df8494aab00504e20e3012e95f067667e41afb
Vishwas Ferwani <166019560+vishwasferwani@users.noreply.github.com>
2026-04-03 12:21:35 +0530

    Add LoginSerializer for user authentication

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -12,3 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
